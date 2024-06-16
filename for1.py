c0054e2dbd697502b8c3c3d1b3b476a97ce51fdd
Jeremy Su <withlove5313@gmail.com>
2024-06-16 13:33:38 +0800

    Add new file.For practice

---
+++ for1.py
@@ -0,0 +1,10 @@
+#For loop
+
+cars = ['Toyota', 'Honda']
+
+for car in cars:
+	print(car)
+
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for studant in students:
+    print('Hi', studant)
